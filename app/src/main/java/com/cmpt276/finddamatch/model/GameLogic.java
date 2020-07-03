e6dcd9ced381912f7ef67fdce8ad53f77e5ab346
Wei Cheng Wang <jwwwcog@gmail.com>
2020-07-02 23:46:16 -0700

    Add shuffleDeck method in GameLogic

--- app/src/main/java/com/cmpt276/finddamatch/model/GameLogic.java
+++ app/src/main/java/com/cmpt276/finddamatch/model/GameLogic.java
@@ -6,24 +6,50 @@ package com.cmpt276.finddamatch.model;
 import android.graphics.drawable.Drawable;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GameLogic {
     private Options options = Options.getInstance();
+    private int numCardsPerSet = options.getNumCardsPerSet();
+    private int numImagesPerCard = options.getNumImagesPerCard();
+    private static final int NUM_SWAPS_IN_SHUFFLE = 50;
 
     // createDeck calls the cardGenerator and places cards into a 2d array of card x image index;
+    // i.e. returns 2d array with each card represented as respective row contents
     private int[][] createDeck() {
         CardGenerator cardGenerator = new CardGenerator();
-        ArrayList<Integer> cards = cardGenerator.create_cards(options.getNumImagesPerCard() - 1);
+        ArrayList<Integer> cards = cardGenerator.create_cards(numImagesPerCard - 1);
 
-        int numImagesPerSet = options.getNumImagesPerSet();
-        int numImagesPerCard = options.getNumImagesPerCard();
-        int[][] deck = new int[numImagesPerSet][numImagesPerCard];
+        int[][] deck = new int[numCardsPerSet][numImagesPerCard];
 
-        for (int i = 0; i < numImagesPerSet; i++) {
+        for (int i = 0; i < numCardsPerSet; i++) {
             for (int j = 0; j < numImagesPerCard; j++) {
-                deck[i][j] = cards.get(i + j);
+                deck[i][j] = cards.get(i * numImagesPerCard + j);
             }
         }
+        return deck;
+    }
+
+    // takes deck array and returns the deck with rows swapped randomly
+    public int[][] shuffleDeck(int[][] deck) {
+        Random rand = new Random();
+
+        // randomly choose rows and swap them
+        int temp;
+        int randRow1, randRow2;
+        for (int i = 0; i < NUM_SWAPS_IN_SHUFFLE; i++) {
+            randRow1 = rand.nextInt(numCardsPerSet);
+            randRow2 = rand.nextInt(numCardsPerSet);
+            while (randRow1 == randRow2) {
+                randRow2 = rand.nextInt(numCardsPerSet);
+            }
+            for (int column = 0; column < deck[0].length; column++) {
+                temp = deck[randRow1][column];
+                deck[randRow1][column] = deck[randRow2][column];
+                deck[randRow2][column] = temp;
+            }
+        }
+
         return deck;
     }
 
--- app/src/main/java/com/cmpt276/finddamatch/model/Options.java
+++ app/src/main/java/com/cmpt276/finddamatch/model/Options.java
@@ -6,10 +6,8 @@ package com.cmpt276.finddamatch.model;
 public class Options {
     private static Options instance;
     private int imageSetIndex;
-    private int[][] imageSet;
-    private static final int NUM_IMAGES_PER_SET = 7;
+    private static final int NUM_CARDS_PER_SET = 7;
     private static final int NUM_IMAGES_PER_CARD = 3;
-    private static final int NUM_IMAGE_SETS = 2;
 
     public static Options getInstance() {
         if(instance == null) {
@@ -18,20 +16,15 @@ public class Options {
         return instance;
     }
     public Options() {
-        imageSetIndex = 0;
-        imageSet = new int[NUM_IMAGE_SETS][NUM_IMAGES_PER_SET];
-        /*
-            for imageSet elements
-                imageSet[i][j] = The corresponding R.drawable.image for [set][image]
-         */
+
     }
 
     public int getNumImagesPerCard() {
         return NUM_IMAGES_PER_CARD;
     }
 
-    public int getNumImagesPerSet() {
-        return NUM_IMAGES_PER_SET;
+    public int getNumCardsPerSet() {
+        return NUM_CARDS_PER_SET;
     }
 
     public int getImageSetIndex() {
