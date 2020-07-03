83f6a3cf1322a687242250a76ffe289aa8f9c986
David Doan <vdoan@sfu.ca>
2020-07-02 20:10:24 -0700

    Completed main menu layout

--- app/src/main/java/com/cmpt276/finddamatch/ui/MainMenuActivity.java
+++ app/src/main/java/com/cmpt276/finddamatch/ui/MainMenuActivity.java
@@ -5,10 +5,17 @@ package com.cmpt276.finddamatch.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import com.cmpt276.finddamatch.R;
 
 public class MainMenuActivity extends AppCompatActivity {
-
+    private ImageView cloud;
+    private Button play;
+    private Button option;
+    private Button score;
+    private Button help;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
