efa167f3435e00aa0683896529cf4ce13ca5ad15
Wei Cheng Wang <jwwwcog@gmail.com>
2020-07-02 22:19:42 -0700

    Add createDeck method in GameLogic

--- app/src/main/java/com/cmpt276/finddamatch/model/GameLogic.java
+++ app/src/main/java/com/cmpt276/finddamatch/model/GameLogic.java
@@ -1,5 +1,38 @@
+/*
+    class which handles the game logic
+ */
 package com.cmpt276.finddamatch.model;
 
+import android.graphics.drawable.Drawable;
+
+import java.util.ArrayList;
+
 public class GameLogic {
-    
+    private Options options = Options.getInstance();
+
+    // createDeck calls the cardGenerator and places cards into a 2d array of card x image index;
+    private int[][] createDeck() {
+        CardGenerator cardGenerator = new CardGenerator();
+        ArrayList<Integer> cards = cardGenerator.create_cards(options.getNumImagesPerCard() - 1);
+
+        int numImagesPerSet = options.getNumImagesPerSet();
+        int numImagesPerCard = options.getNumImagesPerCard();
+        int[][] deck = new int[numImagesPerSet][numImagesPerCard];
+
+        for (int i = 0; i < numImagesPerSet; i++) {
+            for (int j = 0; j < numImagesPerCard; j++) {
+                deck[i][j] = cards.get(i + j);
+            }
+        }
+        return deck;
+    }
+
+    public GameLogic() {
+
+    }
+
+    public boolean isMatch(Drawable image) {
+
+        return true;
+    }
 }
--- app/src/main/java/com/cmpt276/finddamatch/model/Options.java
+++ app/src/main/java/com/cmpt276/finddamatch/model/Options.java
@@ -1,3 +1,6 @@
+/*
+    Singleton class that controls the images that the game will use
+ */
 package com.cmpt276.finddamatch.model;
 
 public class Options {
@@ -5,6 +8,7 @@ public class Options {
     private int imageSetIndex;
     private int[][] imageSet;
     private static final int NUM_IMAGES_PER_SET = 7;
+    private static final int NUM_IMAGES_PER_CARD = 3;
     private static final int NUM_IMAGE_SETS = 2;
 
     public static Options getInstance() {
@@ -20,6 +24,14 @@ public class Options {
             for imageSet elements
                 imageSet[i][j] = The corresponding R.drawable.image for [set][image]
          */
+    }
+
+    public int getNumImagesPerCard() {
+        return NUM_IMAGES_PER_CARD;
+    }
+
+    public int getNumImagesPerSet() {
+        return NUM_IMAGES_PER_SET;
     }
 
     public int getImageSetIndex() {
