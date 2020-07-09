0f9780150682a5a5f4358a87a5fde046fe7ce7d9
Wei Cheng Wang <jwwwcog@gmail.com>
2020-07-08 17:37:32 -0700

    Add thematic styling to help screen

--- app/src/main/java/com/cmpt276/finddamatch/ui/HelpActivity.java
+++ app/src/main/java/com/cmpt276/finddamatch/ui/HelpActivity.java
@@ -5,7 +5,6 @@ package com.cmpt276.finddamatch.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +18,7 @@ public class HelpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
-        Button returnBtn = findViewById(R.id.returnButton);
+        Button returnBtn = findViewById(R.id.btn_help_back);
         returnBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
