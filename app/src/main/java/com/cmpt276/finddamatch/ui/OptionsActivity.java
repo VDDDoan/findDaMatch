62025190e1dce8c8ca0c9fcbb87fe6dcd6336d53
vineshr <vineshr.sfu.ca>
2020-07-11 00:35:14 -0700

    Add change image set

--- app/src/main/java/com/cmpt276/finddamatch/ui/OptionsActivity.java
+++ app/src/main/java/com/cmpt276/finddamatch/ui/OptionsActivity.java
@@ -4,19 +4,25 @@
 package com.cmpt276.finddamatch.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.widgets.ConstraintHorizontalLayout;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.cmpt276.finddamatch.R;
+import com.cmpt276.finddamatch.model.Options;
 
 public class OptionsActivity extends AppCompatActivity {
     private Button imgChange;
     private Button returnBtn;
+    private Options option;
+    private LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +32,24 @@ public class OptionsActivity extends AppCompatActivity {
         imgChange = findViewById(R.id.changeImgBtn);
         returnBtn = findViewById(R.id.returnBtn);
 
+
+
         imgChange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Change integer array set somehow
+                option = option.getInstance();
+                int currentImgSet = option.getImageSetIndex();
+                switch (currentImgSet){
+                    case 0:
+                        option.setImageSetIndex(1);
+                        break;
+                    case 1:
+                        option.setImageSetIndex(0);
+                }
+                Toast.makeText(OptionsActivity.this,"Imgdex: "+ option.getImageSetIndex(),
+                        Toast.LENGTH_SHORT).show();
+
             }
         });
 
@@ -41,4 +61,6 @@ public class OptionsActivity extends AppCompatActivity {
         });
 
     }
+
+
 }
