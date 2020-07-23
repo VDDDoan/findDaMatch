709aa18d462c533eb5988e1295109037866bc60d
vineshr <vineshr.sfu.ca>
2020-07-23 10:55:42 -0700

    Add buttons to change order and draw pile. Currently crashes game activity

--- app/src/main/java/com/cmpt276/finddamatch/ui/OptionsActivity.java
+++ app/src/main/java/com/cmpt276/finddamatch/ui/OptionsActivity.java
@@ -9,6 +9,7 @@ import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.cmpt276.finddamatch.R;
 import com.cmpt276.finddamatch.model.Options;
@@ -20,17 +21,44 @@ public class OptionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
 
-        updateScreen();
-        onClickListener(R.id.btn_imageSet0,R.id.btn_imageSet1, 0);
-        onClickListener(R.id.btn_imageSet1,R.id.btn_imageSet0, 1);
 
+        updateScreen();
+        onClickImgSetLisenter(R.id.btn_imageSet0,R.id.btn_imageSet1, 0);
+        onClickImgSetLisenter(R.id.btn_imageSet1,R.id.btn_imageSet0, 1);
+
+        Button orderButton = findViewById(R.id.orderChangeBtn);
+        orderButton.setOnClickListener(v -> {
+            int[] orderOptions = getResources().getIntArray(R.array.gameOrderSizes);
+            int currentOrder = Options.getInstance().getNumImagesPerCard();
+            for (int i = 0; i < orderOptions.length; i++){
+                if(currentOrder == (orderOptions[i] + 1)){
+                    if (i == orderOptions.length-1){
+                        orderButton.setText(getString(R.string.current_order_size,orderOptions[0]));
+                        Options.getInstance().setNumImagesPerCard(orderOptions[0]+1);
+                    }
+                    else{
+                        orderButton.setText(getString(R.string.current_order_size,orderOptions[i+1]));
+                        Options.getInstance().setNumImagesPerCard(orderOptions[i+1]+1);
+                    }
+
+                }
+            }
+            updateDrawPile();
+            Toast.makeText(this,"Draw Pile Options Updated!",Toast.LENGTH_SHORT).show();
+        });
+
+        Button cardNumButton = findViewById(R.id.numOfCardsBtn);
+        cardNumButton.setOnClickListener(v -> {
+            incrementDrawPile();
+
+        });
 
         ImageView returnBtn = findViewById(R.id.btn_options_back);
         returnBtn.setOnClickListener(v -> finish());
 
     }
 
-    private void onClickListener(int btnImgSet, int btnOtherImgSet, int selectedIndex) {
+    private void onClickImgSetLisenter(int btnImgSet, int btnOtherImgSet, int selectedIndex) {
         Button clickedButton = findViewById(btnImgSet);
         Button otherButton = findViewById(btnOtherImgSet);
 
@@ -43,9 +71,27 @@ public class OptionsActivity extends AppCompatActivity {
 
     }
 
+    private int[] getDrawPileOptions(int numImagesPerCard) {
+        int[] drawPileOptions = getResources().getIntArray(R.array.drawPileOrder2);
+        switch (numImagesPerCard){
+            case 3:
+                drawPileOptions = getResources().getIntArray(R.array.drawPileOrder2);
+                break;
+            case 4:
+                drawPileOptions = getResources().getIntArray(R.array.drawPileOrder3);
+                break;
+            case 6:
+                drawPileOptions = getResources().getIntArray(R.array.drawPileOrder5);
+        }
+        return drawPileOptions;
+    }
+
+
     private void updateScreen() {
         Button btnImgSet0 = findViewById(R.id.btn_imageSet0);
         Button btnImgSet1 = findViewById(R.id.btn_imageSet1);
+        Button orderBtn = findViewById(R.id.orderChangeBtn);
+        Button drawPileBtn = findViewById(R.id.numOfCardsBtn);
 
         int selectedImgIndex = Options.getInstance().getImageSetIndex();
         switch (selectedImgIndex){
@@ -55,6 +101,55 @@ public class OptionsActivity extends AppCompatActivity {
             case 1:
                 btnImgSet1.setBackgroundTintList(ColorStateList.valueOf(getColor(R.color.colorCharcoalLite)));
                 break;
+        }
+
+        orderBtn.setText(getString(R.string.current_order_size,Options.getInstance().getNumImagesPerCard()-1));
+
+        updateDrawPile();
+
+
+    }
+
+    private void updateDrawPile(){
+        Button cardNumButton = findViewById(R.id.numOfCardsBtn);
+
+        int[] drawSizeOptions = getDrawPileOptions(Options.getInstance().getNumImagesPerCard());
+        int currentDrawSize = Options.getInstance().getNumCardsPerSet();
+        boolean isNotAnOption = true;
+
+        for (int i = 0; i < drawSizeOptions.length; i++) {
+            if (currentDrawSize == drawSizeOptions[i]) {
+                cardNumButton.setText(getString(R.string.current_number_of_cards,drawSizeOptions[i]));
+                Options.getInstance().setNumCardsPerSet(drawSizeOptions[i]);
+                isNotAnOption = false;
+            }
+        }
+
+        if (isNotAnOption){
+            cardNumButton.setText(getString(R.string.current_number_of_cards,drawSizeOptions[0]));
+            Options.getInstance().setNumCardsPerSet((drawSizeOptions[0]));
+        }
+    }
+
+    private void incrementDrawPile(){
+        Button cardNumButton = findViewById(R.id.numOfCardsBtn);
+
+        int[] drawSizeOptions = getDrawPileOptions(Options.getInstance().getNumImagesPerCard());
+        int currentDrawSize = Options.getInstance().getNumCardsPerSet();
+
+        for (int i = 0; i < drawSizeOptions.length; i++) {
+            if (currentDrawSize == drawSizeOptions[i]) {
+                if (i == drawSizeOptions.length - 1) {
+                    cardNumButton.setText(getString(R.string.current_number_of_cards, drawSizeOptions[0]));
+                    Options.getInstance().setNumCardsPerSet(drawSizeOptions[0]);
+                } else if (i == drawSizeOptions.length - 2) {
+                    cardNumButton.setText(R.string.drawPileAllOption);
+                    Options.getInstance().setNumCardsPerSet(drawSizeOptions[i + 1]);
+                } else {
+                    cardNumButton.setText(getString(R.string.current_number_of_cards, drawSizeOptions[i + 1]));
+                    Options.getInstance().setNumCardsPerSet(drawSizeOptions[i + 1]);
+                }
+            }
         }
     }
 
