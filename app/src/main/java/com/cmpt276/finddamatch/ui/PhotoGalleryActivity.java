edeec3775ca26885949e4012d7e155b64b820e1a
David Doan <vdoan@sfu.ca>
2020-07-22 23:58:00 -0700

    completed UI of dialog and added it to PhotoGalleryActivity

--- app/src/main/java/com/cmpt276/finddamatch/ui/PhotoGalleryActivity.java
+++ app/src/main/java/com/cmpt276/finddamatch/ui/PhotoGalleryActivity.java
@@ -2,13 +2,43 @@ package com.cmpt276.finddamatch.ui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PhotoGalleryActivity extends AppCompatActivity {
+import com.cmpt276.finddamatch.R;
+
+public class PhotoGalleryActivity extends AppCompatActivity implements DialogFlickrSearch.SearchDialogListener {
+    private String searchWord;
+    private ImageView search;
 
     public static Intent newIntent(Context packageContext){
         Intent intent = new Intent(packageContext, PhotoGalleryActivity.class);
         return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flickr_container);
+        search = findViewById(R.id.search_Button);
+        search.setOnClickListener(v->{
+            showDialog();
+        });
+        showDialog();
+        Toast.makeText(this,searchWord, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onFinishSearchDialog(String inputText) {
+        searchWord = inputText;
+    }
+
+    private void showDialog(){
+        DialogFlickrSearch dialog = new DialogFlickrSearch();
+        dialog.setCancelable(false);
+        dialog.show(getSupportFragmentManager(),"dialog");
     }
 }
